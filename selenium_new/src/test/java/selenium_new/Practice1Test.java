3bdf2916f8512d16f05d84c527f09ff0394c1386
jithu <jithu@DESKTOP-Q1LI5GB>
2022-07-19 11:45:27 +0530

    CREATE GIT

---
+++ selenium_new/src/test/java/selenium_new/Practice1Test.java
@@ -0,0 +1,14 @@
+package selenium_new;
+
+import org.testng.annotations.Test;
+
+public class Practice1Test {
+	@Test
+	public void demo() {
+		System.out.println("Test1--->Test3");
+	}
+	public void demo1() {
+		System.out.println("Test2-->Test3");
+	}
+
+}
---
+++ selenium_new/src/test/java/selenium_new/Practise2Test.java
@@ -0,0 +1,13 @@
+package selenium_new;
+
+import org.testng.annotations.Test;
+
+public class Practise2Test {
+	@Test
+	public void demo1() {
+		System.out.println("Test1-->Test2");
+	}
+	public void demo() {
+		System.out.println();
+	}
+}
