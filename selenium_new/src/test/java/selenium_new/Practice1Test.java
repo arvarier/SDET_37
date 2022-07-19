1866e56de0ed10fbb3defe42d043824b66d32a5c
jithu <jithu@DESKTOP-Q1LI5GB>
2022-07-19 12:08:12 +0530

    update

--- selenium_new/src/test/java/selenium_new/Practice1Test.java
+++ selenium_new/src/test/java/selenium_new/Practice1Test.java
@@ -6,6 +6,7 @@ public class Practice1Test {
 	@Test
 	public void demo() {
 		System.out.println("Test1--->Test3");
+		System.out.println("sample for git");
 	}
 	public void demo1() {
 		System.out.println("Test2-->Test3");
